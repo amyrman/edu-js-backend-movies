2d730eb49016b2f2ba56e57b2d0ab9f7e5ab4e24
Anders Myrman <amyhrman@gmail.com>
2022-03-03 15:04:17 +0100

    Create basic HTTP server with Node

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-import sayHello from './src/hello.js';
+import http from "http";
 
-sayHello('Anders');
+const server = http.createServer((request, response) => {
+  
+  response.statusCode = 200;
+
+  if (request.url == "/hello") {
+    response.write("Hello, world!");
+  } else if (request.url == "/goodbye") {
+    response.write("Goodbye, world!");
+  }
+
+  response.end();
+});
+server.listen(5080);
