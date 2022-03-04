ea0419b1d71f55f8fcdced1ddce396824287e94f
Anders Myrman <amyhrman@gmail.com>
2022-03-04 20:18:28 +0100

    Rename replace-string for maintainability

--- node-dynamic/index.js
+++ node-dynamic/index.js
@@ -3,12 +3,14 @@ import fs from "fs/promises";
 
 const app = express();
 
-// Make server respond dynamically -- through actual user input instead of just serving static HTML files. We're also rendering HTML on the server, that is, changing (replacing) content dynamically, through external(?) manipulation (aka server side rendering, SSR). The equivalent in the browser environment is; using the DOM API. 
+// Make server respond dynamically -- through actual user input instead of just serving static HTML files. We're rendering HTML on the server and changing (replacing) content dynamically, through external(?) manipulation (aka server side rendering, SSR). The equivalent in the browser environment is; using the DOM API.
+
+// Also, we've renamed the string to replace in content variable (from "name" to "$first_name$") , because replace() method replaces only first occurrence of a string. Earlier, the pattern was not unique which led to bad maintanability / separation -- when moving code around, it stopped working.
 
 app.get('/', async (request, response) => {
-  const name = request.query.name || 'user';
+  const first_name = request.query.first_name || 'user';
   const fileBuf = await fs.readFile('./files/index.html');
-  const content = fileBuf.toString().replace('name', name)
+  const content = fileBuf.toString().replace('$first_name$', first_name)
   response.type('html');
   response.send(content);
 });
