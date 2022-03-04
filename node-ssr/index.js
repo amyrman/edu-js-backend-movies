dbdf0c515e177ea74d076abbbdc243d0b30b3b05
Anders Myrman <amyhrman@gmail.com>
2022-03-04 23:09:24 +0100

    Build menu from JS array

--- node-ssr/index.js
+++ node-ssr/index.js
@@ -1,24 +1,49 @@
-import express, { response } from "express";
-import fs from "fs/promises";
+import express from 'express';
+import fs from 'fs/promises';
 
 const app = express();
 
-async function loadHeader() {
+// This, together with menuHtml, is here to demonstrate part of maintainability principle (in a very cumbersome way),
+// i.e. to loop (.map) through an array and render it in HTML to create an easier way to manipulate data/content --
+// To do this as frictionless as possible, in as few places as possible and dynamically / remotely.
+// I guess the reason is to show how and why handlebars extension, later on, is a good alternative.
+const menu = [
+  {
+    label: 'Home',
+    link: '/',
+  },
+  {
+    label: 'About us',
+    link: '/about',
+  },
+  {
+    label: 'Contact us',
+    link: '/contact',
+  },
+];
+
+async function loadHeader(path) {
   const headerBuf = await fs.readFile("./templates/header.html");
   const headerText = headerBuf.toString();
-  return headerText;
+
+  const menuHtml = menu.map(item => {
+    const activeClass = item.link == path? 'active' : 'inactive';
+    return `<li class="${activeClass} menu item"><a href="${item.link}">${item.label}</a></li>`;
+  }).join('\n');
+  
+  return headerText.replace('%menu%', menuHtml);
 }
 
 app.get("/", async (req, res) => {
-  const headerText = await loadHeader();
-  
+  const headerText = await loadHeader('/');
+
   const htmlBuf = await fs.readFile("./templates/index.html");
   const htmlText = htmlBuf.toString().replace("%header%", headerText);
   res.send(htmlText);
 });
 
 app.get("/about", async (req, res) => {
-  const headerText = await loadHeader();
+  const headerText = await loadHeader('/about');
 
   const htmlBuf = await fs.readFile("./templates/about.html");
   const htmlText = htmlBuf.toString().replace("%header%", headerText);
@@ -26,7 +51,7 @@ app.get("/about", async (req, res) => {
 });
 
 app.get("/contact", async (req, res) => {
-  const headerText = await loadHeader();
+  const headerText = await loadHeader('/contact');
 
   const htmlBuf = await fs.readFile("./templates/contact.html");
   const htmlText = htmlBuf.toString().replace("%header%", headerText);
