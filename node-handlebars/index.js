6665e0a54338359bf41c805485986f9c5261673a
Anders Myrman <amyhrman@gmail.com>
2022-03-08 10:58:23 +0100

    Add menu array from /node-ssr

--- node-handlebars/index.js
+++ node-handlebars/index.js
@@ -3,6 +3,20 @@ import fs from "fs/promises"
 
 const app = express();
 
+const menu = [
+  {
+    label: 'Home',
+    link: '/',
+  },
+  {
+    label: 'About us',
+    link: '/about',
+  },
+  {
+    label: 'Contact us',
+    link: '/contact',
+  },
+];
 
 app.get("/", async (req, res) => {
   const htmlBuf = await fs.readFile("./templates/index.html");
