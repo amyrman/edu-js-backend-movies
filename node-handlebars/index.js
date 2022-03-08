a34296db154c12f4da4350ae9e279acb08e722f3
Anders Myrman <amyhrman@gmail.com>
2022-03-08 10:53:11 +0100

    Add basic Express server functionality

--- node-handlebars/index.js
+++ node-handlebars/index.js
@@ -1,6 +1,16 @@
 import express from "express";
+import fs from "fs/promises"
 
 const app = express();
 
+
+app.get("/", async (req, res) => {
+  const htmlBuf = await fs.readFile("./templates/index.html");
+  const htmlText = htmlBuf.toString();
+
+  res.send(htmlText);
+});
+
+app.use("/", express.static("./static"));
 
 app.listen(5080)
