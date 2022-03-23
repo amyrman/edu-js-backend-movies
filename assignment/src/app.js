2c23a8fe2f3a07fa0eeba784b5838e0f5a8223ae
Anders Myrman <amyhrman@gmail.com>
2022-03-23 15:41:20 +0100

    Add route functions to render header menu and movies

--- assignment/src/app.js
+++ assignment/src/app.js
@@ -1,13 +1,16 @@
 import express from "express";
 import { engine } from "express-handlebars";
 import { marked } from "marked";
+import { loadAllMovies, loadMovie } from "./movies.js";
 
 const app = express();
 
 // Add markdown support for use in rendering movie info from API
 app.engine("handlebars", engine({
   helpers: {
-    markdown: md => marked(md),
+    markdown: function (md) {
+      return marked(md);
+    },
   },
 }));
 app.set("view engine", "handlebars");
@@ -28,16 +31,28 @@ const menu = [
   },
 ];
 
+function menuWithActive(path) {
+  return menu.map(function (item) {
+      return {
+        link: item.link,
+        label: item.label,
+        active: item.link == path,
+      };
+    });
+}
+
 app.get('/', async (req, res) => {
-  res.render("index");
+  res.render("index", { menu: menuWithActive(req.path) });
 });
 
-app.get('/allmovies', async (req, res) => {
-  res.render("allmovies", { allmovies });
+app.get('/movies', async (req, res) => {
+  const movies = await loadAllMovies();
+  res.render("allmovies", { movies, menu: menuWithActive(req.path) });
 });
 
-app.get('/onemovie:movieId', async (req, res) => {
-  res.render("onemovie", { onemovie });
+app.get('/movies/:Id', async (req, res) => {
+  const movie = await loadMovie(req.params.Id);
+  res.render("singlemovie", { movie, menu: menuWithActive(req.path) });
 });
 
 
