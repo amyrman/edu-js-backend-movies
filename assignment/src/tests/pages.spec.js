9e8d82b94052c4f0d99e51999aee2af6804df58e
Anders Myrman <amyhrman@gmail.com>
2022-03-24 11:55:25 +0100

    Create integration tests for movies and specific movie pages

--- assignment/src/tests/pages.spec.js
+++ assignment/src/tests/pages.spec.js
@@ -1,8 +1,20 @@
 import request from "supertest";
 import app from "../app.js"
 
-test("home page shows list of movies", async () => {
+// Tests that we would've wished having before 
+test("movie page shows list of movies", async () => {
   const response = await request(app)
-  .get("/")
+  .get("/movies")
   .expect(200);
+
+  expect(response.text.includes('Shawshank')).toBeTruthy();
+  expect(response.text.includes('Godfather')).toBeTruthy();
+})
+
+test("single movie page shows movie data", async () => {
+  const response = await request(app)
+  .get("/movies/1")
+  .expect(200);
+
+  expect(response.text.includes('Shawshank')).toBeTruthy();
 })
